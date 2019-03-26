32dbf816f7f3701461eb882eb3da650916a445f0
unknown <alfypro>
2019-03-26 22:09:33 +0100

    Servicio api

---
+++ src/app/api/servicio.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServicioService {
+  // https://www.fundacionlengua.com/api/test/?token=2ab9586fc31a8e395fe7834b910fe5b784aa7ad60ed67a7c8a1b905fb4f921da
+  constructor() { }
+
+}
